f052481ed8d020c0135e288a9c35367d932676ed
Hibari <jongxiankhoo@gmail.com>
2026-01-28 15:19:31 +0900

    feat: add HTTP client  and configure Module to use sample OneFrame data
    * update executionContext to `ExecutionContext.global`

--- forex-mtl/src/main/scala/forex/Main.scala
+++ forex-mtl/src/main/scala/forex/Main.scala
@@ -5,12 +5,12 @@ import cats.effect._
 import forex.config._
 import fs2.Stream
 import org.http4s.blaze.server.BlazeServerBuilder
+import org.http4s.blaze.client.BlazeClientBuilder
 
 object Main extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] =
-    new Application[IO].stream(executionContext).compile.drain.as(ExitCode.Success)
-
+    new Application[IO].stream(ExecutionContext.global).compile.drain.as(ExitCode.Success)
 }
 
 class Application[F[_]: ConcurrentEffect: Timer] {
@@ -18,9 +18,10 @@ class Application[F[_]: ConcurrentEffect: Timer] {
   def stream(ec: ExecutionContext): Stream[F, Unit] =
     for {
       config <- Config.stream("app")
-      module = new Module[F](config)
+      client <- Stream.resource(BlazeClientBuilder[F](ec).resource)
+      module = new Module[F](config, client)
       _ <- BlazeServerBuilder[F](ec)
-            .bindHttp(config.http.port, config.http.host)
+            .bindHttp(config.http.port, "0.0.0.0")
             .withHttpApp(module.httpApp)
             .serve
     } yield ()
--- forex-mtl/src/main/scala/forex/services/rates/Interpreters.scala
+++ forex-mtl/src/main/scala/forex/services/rates/Interpreters.scala
@@ -3,6 +3,15 @@ package forex.services.rates
 import cats.Applicative
 import interpreters._
 
+import forex.config.ApplicationConfig
+import org.http4s.client.Client
+
 object Interpreters {
+  // TODO: replace dummy data with live Oneframe data
   def dummy[F[_]: Applicative]: Algebra[F] = new OneFrameDummy[F]()
+  def live[F[_]: Applicative](config: ApplicationConfig, client: Client[F]): Algebra[F] = {
+    val _ = config
+    val _ = client
+    new OneFrameDummy[F]()
+  }
 }
